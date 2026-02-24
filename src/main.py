72f06c44881df4cddbc1758560fd6efa86d7c3bc
Adrian-Apostu <apostuadrian6@gmail.com>
2026-02-24 15:46:10 +0200

    created text_node class

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
-def main():
-    print("Hello World")
+from textnode import TextNode
 
-main()
+def main():
+    test = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+    print(test)
+
+
+if __name__ == "__main__":
+    main()
