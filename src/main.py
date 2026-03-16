6e3d1b4f633bc4a186aff58f3d902cff9d69fde8
Adrian <apostuadrian6@gmail.com>
2026-03-16 14:45:23 +0200

    add static file copying functionality and initial CSS styling

--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
+import os
+import shutil
 from textnode import TextNode, TextType
 
+def copy_static(src, dst):
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    os.mkdir(dst)
+    for item in os.listdir(src):
+        current_path=os.path.join(src, item)
+        if os.path.isfile(current_path):
+            shutil.copy(current_path, dst)
+        else:
+            new_dst = os.path.join(dst, item)
+            copy_static(current_path, new_dst)
+
 def main():
-    test = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(test)
+    copy_static("static", "public")
 
 
 if __name__ == "__main__":
